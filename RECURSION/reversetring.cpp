f152893912af4028d320e08a1468338a9a9ffb3d
arshad3e <arshad.3e@gmail.com>
2024-11-05 23:53:03 -0500

    Update reversetring.cpp

--- RECURSION/reversetring.cpp
+++ RECURSION/reversetring.cpp
@@ -6,13 +6,14 @@ void reversestring(string &line, int i=0){
         int size = line.length();
 
         if(i == size/2){ return;} else{
-        swap(line[i], line[size-1-i];
+        swap(line[i], line[size-1-i]);
         reversestring(line, i+1);
 }}
 
 int main(){
 
 string line_1 = "arshad";
-cout << reversestring(line_1);
+reversestring(line_1);
+cout << line_1;
 
 return 0;
