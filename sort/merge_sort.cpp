24c75aae718fd75d7273c7d2d9ea1f591ea4a2f8
arshad3e <arshad.3e@gmail.com>
2024-11-18 21:45:30 -0500

    Update merge_sort.cpp

--- sort/merge_sort.cpp
+++ sort/merge_sort.cpp
@@ -5,71 +5,85 @@ Space Complexity = O(n)
 */
 
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-void mergeArray(int arr[], int low, int mid, int high) {
-    int size1 = mid - low + 1;
-    int size2 = high - mid;
+// Merge two subarrays
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1; // Size of the left subarray
+    int n2 = right - mid;    // Size of the right subarray
 
-    int leftArr[size1], rightArr[size2];
+    int L[n1], R[n2];        // Temporary arrays
 
-    for(int i = 0; i < size1; i++) {
-        leftArr[i] = arr[low + i];
-    }
-    for(int j = 0; j < size2; j++) {
-        rightArr[j] = arr[mid + 1 + j];
-    }
-    int i = 0; // index traversing in leftArray
-    int j = 0; // index traversing in rightArray
-    int k = low; // index traversing in original Array
+    // Copy data to temporary arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
 
-    while(i < size1 && j < size2) {
-        if(leftArr[i] <= rightArr[j]) {
-            arr[k] = leftArr[i];
+    // Merge the temporary arrays back into arr
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-        }
-        else {
-            arr[k] = rightArr[j];
+        } else {
+            arr[k] = R[j];
             j++;
         }
         k++;
     }
-    // inserting remaining elements
-    while(i < size1) {
-        arr[k] = leftArr[i];
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
         k++;
     }
-    while(j < size2) {
-        arr[k] = rightArr[j];
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(int arr[], int low, int high) {  // this function divides the array until it can't be divided into half
+// Merge Sort function
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
 
-    if(low < high) {
-        int mid = (low + (high - 1))/2;
+        // Sort the left half
+        mergeSort(arr, left, mid);
 
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
+        // Sort the right half
+        mergeSort(arr, mid + 1, right);
 
-        mergeArray(arr, low, mid, high);
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
     }
 }
 
+// Function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Main function
 int main() {
-    int arr[] = {10, 9, 8, 7, 6, 5, 5, 4, 4, 3, 3, 2, 1, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {6, 3, 8, 5, 2, 7, 4, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-    mergeSort(arr, 0, n);
+    cout << "Original Array: ";
+    printArray(arr, size);
 
-    cout<<"Sorted Array is: "<<endl;
-    for(int i = 0; i < n; i++) {
-        cout<<arr[i]<<"  ";
-    }
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted Array: ";
+    printArray(arr, size);
 
     return 0;
 }
+
