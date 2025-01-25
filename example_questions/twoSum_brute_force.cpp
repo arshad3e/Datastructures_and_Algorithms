ccaf9bbbd9516a96848c21d0e81e9a6fc51fa908
arshad3e <arshad.3e@gmail.com>
2025-01-24 20:31:12 -0500

    Create twoSum_brute_force.cpp

---
+++ example_questions/twoSum_brute_force.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> two_sum(vector<int>& num_list, int value){
+	int n = num_list.size(); //outer loop to iterate over the array
+	for(int i=0; i<n ; i++){     //inner loop to calculate the actual sum
+		for(int j=i+1; j<n ;j++){
+			if( num_list[i] + num_list[j] == value){
+				return {i,j};
+			}
+		}
+	}
+return {};
+}
+
+int main(){
+
+	vector<int> num_list = {2,7,11,23};
+	int target = 9;
+	vector<int> result = two_sum(num_list,target);
+	cout << "Indicies are" << result[0] << result [1] << endl;
+return 0;
+
+}
