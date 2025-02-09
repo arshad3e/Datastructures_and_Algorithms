941e0ae385b9748ac6169d61097384b7c932f744
arshad3e <arshad.3e@gmail.com>
2025-02-08 23:37:38 -0500

    Update Duplicates_Sorted_array_2.cpp

--- example_questions/Duplicates_Sorted_array_2.cpp
+++ example_questions/Duplicates_Sorted_array_2.cpp
@@ -13,3 +13,8 @@ int removeDuplicates(vector<int>& nums) {
 
     return i;
 }
+
+/*Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).*/
