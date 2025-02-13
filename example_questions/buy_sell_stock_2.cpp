eabd6c975fbfaa97a80aeaca3b90854219bd379a
arshad3e <arshad.3e@gmail.com>
2025-02-12 23:19:02 -0500

    Update buy_sell_stock_2.cpp

--- example_questions/buy_sell_stock_2.cpp
+++ example_questions/buy_sell_stock_2.cpp
@@ -17,3 +17,34 @@
         
     }
 };
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int profit = 0;
+    
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1]; // Add profit from each increase
+        }
+    }
+
+    return profit;
+}
+
+int main() {
+    vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+    cout << "Max Profit: " << maxProfit(prices1) << endl; // Output: 7
+
+    vector<int> prices2 = {1, 2, 3, 4, 5};
+    cout << "Max Profit: " << maxProfit(prices2) << endl; // Output: 4
+
+    vector<int> prices3 = {7, 6, 4, 3, 1};
+    cout << "Max Profit: " << maxProfit(prices3) << endl; // Output: 0
+
+    return 0;
+}
+
+
